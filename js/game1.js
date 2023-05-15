aa9601accf1915c7ff2531fbcd12bf4d07f3fdcd
Unlqsting <noorbijapur500@gmail.com>
2023-05-15 15:24:30 -0700

    more changes but just black screen (nothing wrong with that)

--- js/game1.js
+++ js/game1.js
@@ -1,15 +1,21 @@
-const game = new Phaser.Game(800, 600, Phaser.AUTO, '', 
+const game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', 
     { 
         preload: preload, 
         create: create, 
         update: update 
-    });
+    }
+);
 
 function preload() {
-    game.load.image('character', '../assets/mystic_woods_free_2.1/sprites/characters/player.png')
-    
+    game.load.image('assets/player.png');
+    game.load.image('assets/grass.png')
 }
 
-function create() {}
+function create() {
+    
+    game.add.image(0, 0, 'assets/grass.png');
+    game.add.sprite(100, 100, 'assets/player.png')
+
+}
 
 function update() {}
